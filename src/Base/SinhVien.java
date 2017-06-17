b69201ca470ecfa7d6a36026637f6f09e157862a
thiennvt <thiennvt@localhost>
2017-06-17 13:14:49 +0700

    +demo

---
+++ src/Base/SinhVien.java
@@ -0,0 +1,38 @@
+package Base;
+
+public class SinhVien {
+	private String id;
+	private String name;
+	private String clasName;
+	private String score;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getClasName() {
+		return clasName;
+	}
+	public void setClasName(String clasName) {
+		this.clasName = clasName;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	@Override
+	public String toString() {
+		return "SinhVien [id=" + id + ", name=" + name + ", clasName=" + clasName + ", score=" + score + "]";
+	}
+	
+	
+}
